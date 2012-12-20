bd89c3960761d7be80bf938320ff70e11350a456
Carlo Colombo <carlo.colombo@gmx.com>
2012-12-20 22:48:40 +0100

    download magnet plugin

---
+++ download_magnet.py
@@ -0,0 +1,34 @@
+from contextlib import closing
+import os, re
+
+class DownloadMagnet(object):
+    def __init__(self):
+        self.regex = re.compile("urn:btih:([^&]*)")
+
+    def validator(self):
+        """Return config validator"""
+        from flexget import validator
+        root = validator.factory('root')
+        root.accept('path', allow_replacement=True)
+        root.accept('boolean')
+        return root
+
+    def on_task_output(self,task):
+        config = task.config['download_magnet']
+        for entry in task.entries:
+            if entry.get('urls'):
+                urls = entry.get('urls')
+            else:
+                urls = [entry['url']]
+            for url in [u for u in urls if u.startswith('magnet:')]:
+                content = "d10:magnet-uri%s:%se" % (len(url),url)
+                filename = '%s-%s.torrent' % (entry['title'].replace(' ','_'),self.regex.findall(url)[0])
+                filename = os.path.expanduser(filename)
+                with open(os.path.join(config,filename),'w+') as f:
+                    f.write(content)
+
+try:
+    from flexget.plugin import register_plugin
+    register_plugin(DownloadMagnet, 'download_magnet')
+except:
+    pass
